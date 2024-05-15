28e5f0aa7ad67ddd69434afdcfbe6b61f6cbbabe
mdipsan <93313793+mdipsan@users.noreply.github.com>
2024-05-15 16:00:41 +0300

    add app migration .NET blueprint

---
+++ blueprints/dotNetAppMigrationBlueprint/Rws.LC.AppBlueprint/Enums/AppLifecycleEventEnum.cs
@@ -0,0 +1,40 @@
+﻿namespace Rws.LC.AppBlueprint.Enums
+{
+    public enum AppLifecycleEventEnum
+    {
+        /// <summary>
+        /// App was registered in LanguageCloud.
+        /// </summary>
+        REGISTERED,
+
+        /// <summary>
+        /// App was unregistered in LanguageCloud.
+        /// </summary>
+        UNREGISTERED,
+
+        /// <summary>
+        /// App was activated on a tenant account. This is kept for backwards compatibility for the addons that upgrade from descriptorVersion 1.3 to 1.4
+        /// </summary>
+        ACTIVATED,
+
+        /// <summary>
+        /// App was installed(formerly activated) on a tenant account.
+        /// </summary>
+        INSTALLED,
+
+        /// <summary>
+        /// App version 1.4 was detected in LanguageCloud.
+        /// </summary>
+        UPDATED,
+
+        /// <summary>
+        /// App was deactivated on a tenant account. This is kept for backwards compatibility for the addons that upgrade from descriptorVersion 1.3 to 1.4
+        /// </summary>
+        DEACTIVATED,
+
+        /// <summary>
+        /// App was uninstalled(formerly deactivated) on a tenant account.
+        /// </summary>
+        UNINSTALLED
+    }
+}
---
+++ blueprints/dotNetAppMigrationBlueprint/Rws.LC.AppBlueprint/Exceptions/ConfigurationValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Rws.LC.AppBlueprint.Exceptions
+{
+    public class ConfigurationValidationException : AppException
+    {
+        public ConfigurationValidationException(string message, Details[] details) : base(message)
+        {
+            ErrorCode = ErrorCodes.InvalidConfiguration;
+            StatusCode = HttpStatusCode.BadRequest;
+            ExceptionDetails = details;
+        }
+    }
+}
