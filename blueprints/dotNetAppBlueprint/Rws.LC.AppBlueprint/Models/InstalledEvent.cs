98936b11d7b667bff79b9759e30e85ad7c653b4e
mdipsan <93313793+mdipsan@users.noreply.github.com>
2024-09-11 14:11:58 +0300

    Save tenant region at install - .NET blueprint

---
+++ blueprints/dotNetAppBlueprint/Rws.LC.AppBlueprint/Models/InstalledEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Rws.LC.AppBlueprint.Models
+{
+    public class InstalledEvent
+    {
+        /// <summary>
+        /// The tenant region.
+        /// </summary>
+        public string Region { get; set; }
+    }
+}
