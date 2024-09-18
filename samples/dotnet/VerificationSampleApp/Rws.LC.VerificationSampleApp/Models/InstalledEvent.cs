b433406eb4ea560e54f628343c29d6af51d4b129
mdipsan <93313793+mdipsan@users.noreply.github.com>
2024-09-18 11:51:18 +0300

    Save tenant region at install - .NET Verification sample

---
+++ samples/dotnet/VerificationSampleApp/Rws.LC.VerificationSampleApp/Models/InstalledEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Rws.LC.VerificationSampleApp.Models
+{
+    public class InstalledEvent
+    {
+        /// <summary>
+        /// The tenant region.
+        /// </summary>
+        public string Region { get; set; }
+    }
+}
